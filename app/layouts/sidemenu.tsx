d9741f75f19586963b24bbeb8a9c52732a54e528
kohei-miyadai07 <wm25gb87@gmail.com>
2025-08-04 23:29:18 +0900

    feat: チュートリアル完成

--- app/layouts/sidemenu.tsx
+++ app/layouts/sidemenu.tsx
@@ -1,15 +1,18 @@
+import { List, SearchIcon, TrendingUp } from "lucide-react";
 import { Link, Outlet } from "react-router";
 
-const menuItems = [
-  { name: "記事一覧", path: "/" },
-  { name: "人気記事", path: "/popular" },
-  { name: "検索", path: "/search" },
-];
+export default function Sidemenu() {
+  const menuItems = [
+    { name: "記事一覧", path: "/", icon: List },
+    { name: "人気記事", path: "/popular", icon: TrendingUp },
+    { name: "記事検索", path: "/search", icon: SearchIcon },
+  ];
 
-const sidemenu = () => {
   return (
     <div>
-      <aside className="fixed left-0 top-0 z-40 h-screen w-64 bg-white shadow-lg">
+      <aside
+        className={`fixed left-0 top-0 z-40 h-screen w-64 bg-white shadow-lg`}
+      >
         <div>
           <div>
             <nav>
@@ -19,6 +22,7 @@ const sidemenu = () => {
                   to={item.path}
                   className="flex items-center px-4 py-3 text-gray-700 hover:bg-gray-100"
                 >
+                  <item.icon className="mr-3 h-5 w-5" />
                   {item.name}
                 </Link>
               ))}
@@ -29,6 +33,4 @@ const sidemenu = () => {
       <Outlet />
     </div>
   );
-};
-
-export default sidemenu;
+}
